997334e3894ef7ff579012912577f6b7f9c3ffb2
Uzair Khan <paf_uzair@yahoo.com>
2024-09-19 17:04:38 +0200

    chore: modified Selectors file

--- src/main/java/com/playwright/practice/selectors/Selectors.java
+++ src/main/java/com/playwright/practice/selectors/Selectors.java
@@ -9,7 +9,9 @@ public enum Selectors {
     DASHBOARD_NEWS_SECTION("news-section"),
     DASHBOARD_PUBLISHED_TASKS_SECTION("published-tasks-section"),
     DASHBOARD_DRAFT_TASK_SECTION("private-tasks-section"),
-    INVALID_LOGIN_NOTIFICATION("notification");
+    INVALID_LOGIN_NOTIFICATION("notification"),
+    COURSE_OVERVIEW_SIDE_MENU_BAR_SEL("Course-Overview"),
+    COURSE_OVERVIEW_SEARCH_FIELD_SEL("search-field-course");
 
     private final String selector;
 
