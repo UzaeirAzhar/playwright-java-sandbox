84948242592898e8127dd159deea98a3fe7f979b
Uzair Khan <paf_uzair@yahoo.com>
2024-10-02 01:15:13 +0200

    chore: update PageFactory file

--- src/main/java/com/playwright/practice/utils/PageFactory.java
+++ src/main/java/com/playwright/practice/utils/PageFactory.java
@@ -2,9 +2,6 @@ package com.playwright.practice.utils;
 
 import com.microsoft.playwright.Page;
 import com.playwright.practice.pages.BasePage;
-import com.playwright.practice.pages.CourseOverviewPage;
-import com.playwright.practice.pages.DashboardPage;
-import com.playwright.practice.pages.LoginPage;
 
 public class PageFactory {
     public static <T extends BasePage> T getPage(Class<T> pageClass, Page page) {
